6fc1e3945882cce36f60aec43e28f39c5cb1a10d
John <john@JohndeMacBook-Air.local>
2026-05-13 19:51:43 +0300

    fix: correct IIFE invocation in TeamTasks so displayed tasks render properly

--- src/pages/TeamTasks.tsx
+++ src/pages/TeamTasks.tsx
@@ -406,7 +406,8 @@ export default function TeamTasks() {
                         );
                     })}
                 </div>
-            )})}
+            );
+        })()}
 
             {/* Create Task Modal */}
             {showCreateModal && (
