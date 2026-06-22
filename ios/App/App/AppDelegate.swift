b0130a8b2f6809621d9d48427b815a56f1e63dcb
John <john@JohndeMacBook-Air.local>
2026-06-22 13:34:16 +0300

    fix: use type-safe Notification.Name properties in AppDelegate remote notifications

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -16,16 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
+
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let tokenString = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
         print("[AppDelegate] didRegisterForRemoteNotificationsWithDeviceToken called! Token: \(tokenString)")
-        NotificationCenter.default.post(name: Notification.Name(rawValue: "capacitorDidRegisterForRemoteNotifications"), object: deviceToken)
+        print("[AppDelegate] Posting notification: \(Notification.Name.capacitorDidRegisterForRemoteNotifications.rawValue)")
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
     }
 
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("[AppDelegate] didFailToRegisterForRemoteNotificationsWithError called! Error: \(error.localizedDescription) - Detail: \(error)")
-        NotificationCenter.default.post(name: Notification.Name(rawValue: "capacitorDidFailToRegisterForRemoteNotifications"), object: error)
+        print("[AppDelegate] Posting notification error: \(Notification.Name.capacitorDidFailToRegisterForRemoteNotifications.rawValue)")
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
     }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
